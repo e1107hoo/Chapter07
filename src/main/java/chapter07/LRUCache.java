847576eebc08daaef47f14e06a3b53cf46d0d865
e1107hoo <ooe@e-mind.co.jp>
2013-06-19 08:41:21 +0900

    LRUCacheTest作成　テストをしたがLRUCacheが未実装なのでテスト失敗

--- src/main/java/chapter07/LRUCache.java
+++ src/main/java/chapter07/LRUCache.java
@@ -1,13 +1,40 @@
-/**
- * 
- */
 package chapter07;
 
+import java.util.ArrayList;
 
 /**
  * @author mind
+ * @param <E> 
  *
  */
-public class LRUCache {
+public class LRUCache<E> {
+	
+	final ArrayList<String>	keys	= new ArrayList<String>();
+	
+	
+	/**
+	 * @param string
+	 * @param string2
+	 */
+	public void put(String key, E item) {
+		
+	}
+	
+	/**
+	 * @return
+	 */
+	public Integer size() {
+		// TODO 自動生成されたメソッド・スタブ
+		return 2;
+	}
+	
+	/**
+	 * @param string
+	 * @return
+	 */
+	public String get(String key) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
 	
 }
--- src/test/java/chapter07/LRUCacheTest.java
+++ src/test/java/chapter07/LRUCacheTest.java
@@ -3,20 +3,78 @@
  */
 package chapter07;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
-
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
 /**
  * @author mind
  *
  */
-public class LRUCacheTest extends LRUCache {
+@RunWith(Enclosed.class)
+public class LRUCacheTest {
 	
-	@Test
-	public void test() {
-		fail("まだ実装されていません");
+	public static class AとBを追加している場合 {
+		
+		LRUCache<String>	sut;
+		
+		
+		@Before
+		public void setup() throws Exception {
+			sut = new LRUCache<String>();
+			sut.put("A", "valueA");
+			sut.put("A", "valueB");
+		}
+		
+		@Test
+		public void sizeは2() throws Exception {
+			assertThat(sut.size(), is(2));
+		}
+		
+		@Test
+		public void get_AでvalueAを返しkeysはBA() throws Exception {
+			assertThat(sut.get("A"), is("valueA"));
+			assertThat(sut.keys.size(), is(2));
+			assertThat(sut.keys.get(0), is("B"));
+			assertThat(sut.keys.get(1), is("A"));
+		}
+		
+		@Test
+		public void get_BでvalueBを返しkeysはAB() throws Exception {
+			assertThat(sut.get("B"), is("valueB"));
+			assertThat(sut.keys.size(), is(2));
+			assertThat(sut.keys.get(0), is("A"));
+			assertThat(sut.keys.get(1), is("B"));
+		}
+		
+		@Test
+		public void get_Cでnullを返しkeysはAB() throws Exception {
+			assertThat(sut.get("C"), is(nullValue()));
+			assertThat(sut.keys.size(), is(2));
+			assertThat(sut.keys.get(0), is("A"));
+			assertThat(sut.keys.get(1), is("B"));
+		}
+		
+		@Test
+		public void put_Cでsizeは3_keysはABCとなる() throws Exception {
+			// Set up
+			String key = "C";
+			String item = "valueC";
+			// Exercise
+			sut.put(key, item);
+			// Verify
+			assertThat(sut.size(), is(3));
+			assertThat(sut.keys.size(), is(3));
+			assertThat(sut.keys.get(0), is("A"));
+			assertThat(sut.keys.get(1), is("B"));
+			assertThat(sut.keys.get(2), is("C"));
+		}
+		
 	}
 	
 }
